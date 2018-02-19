4b2339bbecc11070dce047f58be3a038d19ef052
Jatupon Jantri <jameball007@gmail.com>
2018-02-19 16:52:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  window.addEventListener
+  window.addEventListener("keydown", function(event) {
+    var key = parseInt(e.detail || e.which)
+  })
   var key = parseInt(e.detail || e.which)
             if (key === code[index]) {
                 index++
